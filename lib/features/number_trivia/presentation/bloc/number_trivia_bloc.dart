a80380db461c23aa533c6d2e066d6cbcdda71b71
Ivan Shuvalov <ivan.shuvalov@colba.es>
2022-02-23 18:08:01 +0100

    Part 11 Broken: Bloc Fix needed

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,14 +1,43 @@
 import 'package:bloc/bloc.dart';
+import 'package:course2/core/util/input_converter.dart';
 import 'package:course2/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:course2/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:course2/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:equatable/equatable.dart';
 
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
+const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+const String CACHE_FAILURE_MESSAGE = 'Cache Failure';
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    'Invalid Input - The number must be positive integer';
+
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc() : super(Empty()) {
-    on<NumberTriviaEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc(
+      {required this.getConcreteNumberTrivia,
+      required this.getRandomNumberTrivia,
+      required this.inputConverter})
+      : super(Empty()) {
+    on<NumberTriviaEvent>((event, emit) {});
+  }
+
+  @override
+  NumberTriviaState get initialState => Empty();
+
+  @override
+  Stream<NumberTriviaState> mapEventToState(NumberTriviaEvent event) async* {
+    if (event is GetTriviaForConcreteNumber) {
+      final inputEither =
+          inputConverter.stringToUnsignedInteger(event.numberString);
+
+      yield* inputEither.fold((l) async* {
+        yield Error(message: INVALID_INPUT_FAILURE_MESSAGE);
+      }, (integer) => throw UnimplementedError());
+    }
   }
 }
