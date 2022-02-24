269bcf0388ad50d9d8e218448381356acb88a7b6
Ivan Shuvalov <ivan.shuvalov@colba.es>
2022-02-24 10:40:39 +0100

    Branch initial commit

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,12 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:course2/core/util/input_converter.dart';
 import 'package:course2/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:course2/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:course2/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:equatable/equatable.dart';
 
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc() : super(Empty()) {
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+  NumberTriviaBloc(
+      {required this.getConcreteNumberTrivia,
+      required this.getRandomNumberTrivia,
+      required this.inputConverter})
+      : super(Empty()) {
     on<NumberTriviaEvent>((event, emit) {
       // TODO: implement event handler
     });
---
+++ test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
@@ -0,0 +1,71 @@
+import 'package:course2/core/util/input_converter.dart';
+import 'package:course2/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:course2/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:course2/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'package:course2/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'number_trivia_bloc_test.mocks.dart';
+
+@GenerateMocks([], customMocks: [
+  MockSpec<GetConcreteNumberTrivia>(
+      as: #MockGetConcreteNumberTrivia, returnNullOnMissingStub: true),
+  MockSpec<GetRandomNumberTrivia>(
+      as: #MockGetRandomNumberTrivia, returnNullOnMissingStub: true),
+  MockSpec<InputConverter>(
+      as: #MockInputConverter, returnNullOnMissingStub: true)
+])
+void main() {
+  final mockGetConcreteNumberTrivia = MockGetConcreteNumberTrivia();
+  final mockGetRandomNumberTrivia = MockGetRandomNumberTrivia();
+  final mockInputConverter = MockInputConverter();
+  final bloc = NumberTriviaBloc(
+      getConcreteNumberTrivia: mockGetConcreteNumberTrivia,
+      getRandomNumberTrivia: mockGetRandomNumberTrivia,
+      inputConverter: mockInputConverter);
+
+  test('initialState should be Empty', () {
+    // assert
+    expect(bloc.state, Empty());
+  });
+
+  group('GetTriviaForConcreteNumber', () {
+    const tNumberString = '1';
+    const tNumberParsed = 1;
+    final tNumberTrivia = NumberTrivia(text: 'test text', number: 1);
+
+    test(
+      'should call the InputConverter to validate and convert the string to an unsigned integer',
+      () async {
+        // arrange
+        when(mockInputConverter.stringToUnsignedInteger(any))
+            .thenReturn(Right(tNumberParsed));
+        // act
+        bloc.add(GetTriviaForConcreteNumber(tNumberString));
+        await untilCalled(mockInputConverter.stringToUnsignedInteger(any));
+        // assert
+        verify(mockInputConverter.stringToUnsignedInteger(tNumberString));
+      },
+    );
+
+    test(
+      'should emit [Error] when the input is invalid',
+      () async {
+        // arrange
+        when(mockInputConverter.stringToUnsignedInteger(any))
+            .thenReturn(Left(InvalidInputFailure()));
+        // assert later
+        final expected = [
+          Empty(),
+          Error(message: "INVALID_INPUT_FAILURE_MESSAGE"),
+        ];
+        expectLater(bloc.state, emitsInOrder(expected));
+        // act
+        bloc.add(GetTriviaForConcreteNumber(tNumberString));
+      },
+    );
+  });
+}
